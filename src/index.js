414862162b863127a6d42d77225648e008702cb0
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-03-14 13:58:47 +0100

    added stackgrid compontent, search stub

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-//console.log('My Minimal React Webpack Babel Setup');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GridComponent from './stackgrid';
+//import SearchComponent from './search';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
+
+function App() {
+    return (
+        <div>
+            <div>{title}</div>
+            
+            <GridComponent />
+        </div>
+    );
+  }
+
+  const styleLink = document.createElement("link");
+  styleLink.rel = "stylesheet";
+  styleLink.href = "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css";
+  document.head.appendChild(styleLink);
+
 ReactDOM.render(
-    <div>{title}</div>,
+    <App />,
     document.getElementById('app')
 );
