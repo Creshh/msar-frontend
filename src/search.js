aeda917beb76c8356380cb56e0fec2d032ab79a8
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-03-14 15:54:08 +0100

    working suggestion

--- src/search.js
+++ src/search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash'
 import { Search } from 'semantic-ui-react'
 
 export default class SearchComponent extends React.Component {
@@ -16,29 +17,23 @@ export default class SearchComponent extends React.Component {
     
     handleSearchChange(e, { value }){
         this.setState({ isLoading: true, value })
-    
-        // setTimeout(() => {
-        //   if (this.state.value.length < 1) return this.resetComponent()
-    
-        //   const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
-        //   const isMatch = result => re.test(result.title)
-    
-        //   const filteredResults = _.reduce(
-        //     source,
-        //     (memo, data, name) => {
-        //       const results = _.filter(data.results, isMatch)
-        //       if (results.length) memo[name] = { name, results } // eslint-disable-line no-param-reassign
-    
-        //       return memo
-        //     },
-        //     {},
-        //   )
-    
-        //   this.setState({
-        //     isLoading: false,
-        //     results: filteredResults,
-        //   })
-        // }, 300)
+
+        if (!value){
+            this.resetComponent()
+            return
+        }
+
+        fetch('/api/suggest?prefix=' + value)
+            .then(response => {
+                return response.json()})
+            .then(json => {
+                console.log('called api')
+                console.log('suggestions: \n' + JSON.stringify(json, null, 2))
+                return json})
+            .then(json => this.setState({
+                isLoading: false,
+                results: json,
+            }))
       }
     
       render() {
@@ -48,8 +43,8 @@ export default class SearchComponent extends React.Component {
               <Search
                 category
                 loading={isLoading}
-                onResultSelect={this.handleResultSelect}
-                onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+                onResultSelect={this.handleResultSelect.bind(this)}
+                onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true }).bind(this)}
                 results={results}
                 value={value}
                 {...this.props}
