aad39c9bd13e8d88f3352063dbd9bde138c03a66
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-03-15 14:39:05 +0100

    use asset api to show first images in grid

--- src/stackgrid.js
+++ src/stackgrid.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import StackGrid from 'react-stack-grid';
+import StackGrid, { transitions, easings } from 'react-stack-grid';
 
+const transition = transitions.scaleDown;
 
 const images = [
-    { src: '../test_images/Theaterplatz Panorama1.jpg', label: 'Theaterplatz Panorama1' },
-    { src: '../test_images/IMG_7628.JPG', label: 'IMG_7628.JPG' },
-    { src: '../../test_images/IMG_8600.jpg', label: 'IMG_8600.jpg' }
+    { src: 'http://localhost:8090/assets/get/1A5A5092a_Jacob_Mueller_1.jpg', label: '1A5A5092a_Jacob_Mueller_1' },
+    { src: 'http://localhost:8090/assets/get/Theaterplatz Panorama1.jpg', label: 'Theaterplatz Panorama1' },
+    { src: 'http://localhost:8090/assets/get/IMG_7628.JPG', label: 'IMG_7628.JPG' },
+    { src: 'http://localhost:8090/assets/get/IMG_8600.jpg', label: 'IMG_8600.jpg' }
   ];
 
 export default class GridComponent extends React.Component {
@@ -20,16 +22,28 @@ export default class GridComponent extends React.Component {
 
     render() {
         return (
-            <StackGrid columnWidth={150}>
-                {images.map(obj => (
-                    <figure
-                        key={obj.src}
-                        className="image"
-                    >
-                        <img src={obj.src} alt={obj.label} />
-                        <figcaption>{obj.label}</figcaption>
-                    </figure>
-                    ))}
+            <StackGrid
+                monitorImagesLoaded
+                columnWidth={300}
+                duration={600}
+                gutterWidth={15}
+                gutterHeight={15}
+                easing={easings.cubicOut}
+                appearDelay={60}
+                appear={transition.appear}
+                appeared={transition.appeared}
+                enter={transition.enter}
+                entered={transition.entered}
+                leaved={transition.leaved}>
+                    {images.map(obj => (
+                        <figure
+                            key={obj.src}
+                            className="image"
+                        >
+                            <img src={obj.src} alt={obj.label} />
+                            <figcaption>{obj.label}</figcaption>
+                        </figure>
+                        ))}
             </StackGrid>
         );
     }
