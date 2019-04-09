bab1ae424a9579b3b4a826867e27714ec1b27b9b
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-04-09 14:41:28 +0200

    search mockup stub

--- src/index.js
+++ src/index.js
@@ -2,26 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GalleryComponent from './components/Gallery';
 import UploadComponent from './components/UploadComponent';
-import Mockup from './Mockup_Landing';
+import Mockup from './Mockup_Search';
 
 const title = 'Meta Search Application';
 
-
-function App() {
-    return (
-        <div className="App">
-            <div>{title}</div>
-            <GalleryComponent />
-
-            <UploadComponent />
-        </div>
-    );
-  }
-
-  const styleLink = document.createElement("link")
-  styleLink.rel = "stylesheet"
-  styleLink.href = "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-  document.head.appendChild(styleLink)
+const styleLink = document.createElement("link")
+styleLink.rel = "stylesheet"
+styleLink.href = "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+document.head.appendChild(styleLink)
 
 ReactDOM.render(
     <div id='index'>
