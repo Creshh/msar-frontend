219a5eaf26edd22e6df54b865cb41642a14a38e0
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-04-10 10:25:38 +0200

    Added Filter mockups

--- src/Mockup_Search.js
+++ src/Mockup_Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import {
     Button,
     Container,
@@ -16,23 +17,91 @@ import {
     Visibility,
     Rail,
     Sticky,
+    Accordion,
+    Input,
+    Label,
   } from 'semantic-ui-react'
 
 export default class Mockup extends React.Component {
 
     constructor(props){
         super(props)
-        this.state = { activeItem: 'search' }
+        this.state = { activeItem: 'search', activeIndex: []}
         this.handleItemClick = this.handleItemClick.bind(this)
+        // this.handleSidebarClick = this.handleSidebarClick.bind(this)
     }
 
     handleItemClick(e, { name }) {
         this.setState({ activeItem: name })
     }
 
+    // handleSidebarClick(e, titleProps) {
+    //     console.log('sidebar click');
+        
+    //     const { index } = titleProps
+    //     console.log(index)
+    //     const { activeItem, activeIndex } = this.state
+    //     const filtered = activeIndex.filter((value) => {return value == index})
+    //     // const newIndex = activeIndex === index ? -1 : index
+    //     console.log(filtered)
+    //     this.setState({ activeIndex: filtered })
+    //   }
+
     render() {
 
-        const { activeItem } = this.state
+        const { activeItem, activeIndex } = this.state
+
+        // const panels = _.times(3, i => ({
+        //     key: `panel-${i}`,
+        //     title: `title-${i}`,
+        //     content: `content-${i}`,
+        //   }))
+        const panels = [3]
+
+        panels[0] = {
+            key: 'tags',
+            title: 'Manual Tags',
+            content:
+                <div className='filter'>
+                    <Input fluid label='AddTag'/>
+                    <Label.Group color='olive'>
+                        <Label as='a'>
+                            Fun
+                            <Icon name='close' />
+                        </Label>
+                        <Label as='a'>
+                            Fun
+                            <Icon name='close' />
+                        </Label>
+                </Label.Group>
+                </div>,
+        }
+
+        panels[1] = {
+            key: 'exif',
+            title: 'EXIF',
+            content:
+                <div className='filter'>
+                    <Input fluid label='Width' />
+                    <Input fluid label='Height' />
+                    <Input fluid label='Persons' type='range' min={0} max={10}  /> 
+                </div>,
+        }
+
+        panels[2] = {
+            key: 'location',
+            title: 'Location',
+            content:
+                <div className='filter'>
+                    <Input fluid label='Country'  />
+                    <Input fluid label='City' />
+                    <Input fluid label='Street' />
+                    <Divider horizontal>OR</Divider>
+                    <Input fluid label='Latitude' />
+                    <Input fluid label='Longitude' />
+                    <Input fluid label='Radius' />
+                </div>,
+        }
 
         return (
             <div id='page'>
@@ -55,41 +124,47 @@ export default class Mockup extends React.Component {
                                     Upload
                             </Menu.Item>
                     </Menu>
-                    <Divider/>
+                    {/* <Divider/> */}
 
                     <Search input={{ fluid: true }} className='secondarySearch'
                         size='small'
                         category
                     />
 
-                    <div className='grid'>
-                        <div className='grid-filters'>
-                        bla <br/>
-                        bla <br/>
-                        bla <br/>
-                        bla <br/>
-                        bla <br/>
-                        </div>
-                        <div className='grid-images'>
-                        blubb
-                        </div>
-                    </div> 
-                    
-                {/* <div className='grid'>
-                    <Menu attached='top'>
+                    <Sidebar.Pushable as={Segment}>
+                            <Sidebar
+                                as={Accordion}
+                                animation='push'
+                                icon='labeled'
+                                exclusive={false}
+                                onHide={this.handleSidebarHide}
+                                visible={true}
+                                // width=''
+                                fluid
+                            >
+                                {/* <Accordion.Title active={true} index={0} onClick={this.handleSidebarClick}>
+                                <Icon name='dropdown' />
+                                What is a dog?
+                                </Accordion.Title>
+                                <Accordion.Content active={true}>
+                                <p>
+                                    A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can
+                                    be found as a welcome guest in many households across the world.
+                                </p>
+                                </Accordion.Content> */}
+                                
+                                
+                                <Accordion defaultActiveIndex={Array.from(Array(panels.length).keys())} panels={panels} exclusive={false} fluid />
+                            </Sidebar>
 
-                        <Menu.Menu >
-                            <Search input={{ fluid: true }} className='secondarySearch'
-                            size='small'
-                            category
-                            />
-                        </Menu.Menu>
-                        </Menu>
-
-                        <Segment attached='bottom'>
-                            blabla
-                        </Segment>
-                        </div> */}
+                            <Sidebar.Pusher>
+                                <Segment basic>
+                                <Header as='h3'>Application Content</Header>
+                                    blablubb
+                                </Segment>
+                            </Sidebar.Pusher>
+                        </Sidebar.Pushable>
+                            
                 </Segment>
 
 
