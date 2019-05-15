509995f378a96480319f7bd29ebe8802803fdfe3
Tom Kretzschmar <tom.kretzschmar@s2015.tu-chemnitz.de>
2019-05-15 09:48:37 +0200

    added UploadComponent; added functions to image grid

--- src/components/ImageGridComponent.js
+++ src/components/ImageGridComponent.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import StackGrid, { transitions, easings } from 'react-stack-grid';
-import {Popup, Image, Button, Modal, Tab} from 'semantic-ui-react'
+import {Popup, Image, Button, Modal, Tab, Message} from 'semantic-ui-react'
 import ReactJson from 'react-json-view'
 import QueryHandler from '../common/QueryHandler'
 import {theme} from '../common/Constants'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 const transition = transitions.scaleDown;
-
+const COPY_TEXT = 'Copy Link'
 
 export default class ImageGridComponent extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {open: false, reference: ''}
+        this.state = {open: false, reference: '', copyText: COPY_TEXT, showMsg: false}
         this.showImage = this.showImage.bind(this)
         this.showMeta = this.showMeta.bind(this)
         this.close = this.close.bind(this)
+        this.onCopy = this.onCopy.bind(this)
+        this.onFilesAdded = this.onFilesAdded.bind(this)
+        this.clickUpload = this.clickUpload.bind(this)
+
+        this.fileInputRef = React.createRef()
     }
 
     showImage(reference){
@@ -26,25 +32,70 @@ export default class ImageGridComponent extends React.Component {
     showMeta(reference){
         const panes = []
         QueryHandler.getDocuments(reference)
-            .then(documents => {
-                console.log(documents)
-                for(let [key, value] of Object.entries(documents)) {
-                    panes.push({menuItem: key, render: () => <ReactJson src={value} enableClipboard={false} displayObjectSize={false} displayDataTypes={false} theme={theme}/>})
+            .then(docs => {
+                console.log(docs)
+                for(let [key, list] of Object.entries(docs)) {
+                    for (let doc of list) {
+                        console.log(doc)
+                        panes.push({menuItem: key + ' from ' + doc.source, render: () => <ReactJson src={doc} enableClipboard={false} displayObjectSize={false} displayDataTypes={false} theme={theme}/>})
+                    }
                 }
                 this.setState({openMeta: true, reference: reference, panes: panes})
             })
     }
 
+    onCopy(){
+        this.setState({copyText: 'Copied!'})
+    }  
+
     close(){
         this.setState({openImage: false, openMeta: false, reference: ''})
     }
 
+    clickUpload(reference){
+        this.setState({uploadRef: reference})
+        this.fileInputRef.current.click()
+    }
+
+    onFilesAdded(e){
+        const {uploadRef} = this.state
+        console.log(uploadRef)
+        const files = e.target.files;
+        console.log(files)
+
+        
+        this.setState({success: false, msg: ''})
+        
+        // if(!file) {
+        //     return {success: false, msg: 'Please upload a file.'}
+        // }
+        // if(file.size >= 50000000) {
+        //     return {success: false, msg: 'File size exceeds limit of 50MB.'}
+        // }
+        
+        QueryHandler.addDocument(files[0], uploadRef)
+            .then(result => {
+                this.setState({success: result.success, msg: result.msg, showMsg: true})
+                setTimeout(() => {
+                    this.setState({showMsg: false})
+                }, result.success ? 3000 : 10000)
+            })
+    }
+
     render() {
         const image_array = this.props.images
-        const {openImage, openMeta, reference, panes} = this.state
+        const {openImage, openMeta, reference, panes, copyText, showMsg, success, msg} = this.state
         
         return (
         <div>
+             <input
+                hidden
+                ref={this.fileInputRef}
+                className="FileInput"
+                type="file"
+                onChange={this.onFilesAdded}
+            />
+
             <Modal dimmer open={openImage} onClose={this.close} basic size='fullscreen'>
                 <Modal.Content image>
                     <Image wrapped src={QueryHandler.getAsset(reference, false)} />
@@ -73,22 +124,40 @@ export default class ImageGridComponent extends React.Component {
                     {image_array.map(obj => (
                         <div key={obj.reference} className='imageContainer'>
                             {/* <Label className='imageLabel' attached='bottom'>{obj.type}</Label> */}
+                            
                             <Image className='gridImage' src={QueryHandler.getAsset(obj.reference, true)} alt={obj.type}/>
                             <div className='imageLabel'>
-                            <Button.Group floated='right'>
-                                
-                                <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='expand' alt='Show Image' onClick={() => this.showImage(obj.reference)}/>} content='Show Image' />
-                                <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='file alternate outline' onClick={() => this.showMeta(obj.reference)}/>} content='Show Metadata' />
-                                <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='add'/>} content='Add Metadata' /> {/*file code*/}
-                                <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='download'/>} content='Download Image' /> {/*upload*/}
-                                <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='linkify'/>} content='Copy Link' /> {/*share alternate*/}
-                                 
-                            </Button.Group>
+                                <Button.Group floated='right'>
+                                    
+                                    <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='expand' alt='Show Image' onClick={() => this.showImage(obj.reference)}/>} content='Show Image' />
+                                    <Popup inverted position='top center' size='tiny' trigger={<Button className='imageButton' color='black' icon='file alternate outline' onClick={() => this.showMeta(obj.reference)}/>} content='Show Metadata' />
+                                    
+                                    <Popup inverted position='top center' size='tiny' trigger={
+                                        <Button className='imageButton' color='black' icon='add' onClick={() => this.clickUpload(obj.reference)}/>
+
+                                    } content='Add Metadata' /> {/*file code*/}
+                                    
+                                    <Popup inverted position='top center' size='tiny' trigger={
+                                        <Button className='imageButton' color='black' icon='download' onClick={() => QueryHandler.downloadAsset(obj.reference)} />
+                                    } content='Download Image' /> {/*upload*/}
+                                    
+                                    <Popup className='imageLabelPopup' inverted position='top center' size='tiny' trigger={
+                                        <CopyToClipboard text={'http://localhost:8080/' + QueryHandler.getAsset(obj.reference, false)} onCopy={() => this.onCopy()}>            
+                                            <Button className='imageButton' color='black' icon='linkify' />
+                                        </CopyToClipboard>
+                                    } content={copyText} onClose={() => this.setState({copyText: COPY_TEXT})}/> {/*share alternate*/}
+                                    
+                                </Button.Group>
                             </div>
                         </div>
                         ))}
             </StackGrid>
+
+            <div className={showMsg ? 'messageBox' : 'messageBox hidden'}>
+                <Message content={msg} positive={success} negative={!success}/>
             </div>
+
+        </div>
         );
     }
 }
---
+++ src/components/UploadComponent.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import {Popup, Image, Button, Modal, Tab, Card, Message} from 'semantic-ui-react'
+import ReactJson from 'react-json-view'
+import QueryHandler from '../common/QueryHandler'
+
+
+export default class UploadComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {success: false, msg: '', showMsg: false, typeList: {}}
+        this.onFilesAdded = this.onFilesAdded.bind(this)
+        this.clickUpload = this.clickUpload.bind(this)
+
+        this.fileInputRef = React.createRef()
+
+    }
+
+    componentDidMount(){
+        QueryHandler.getTypes()
+        .then(types => {
+            console.log(types)
+            this.setState({typeList: types})
+
+            // for (let [key, value] of Object.entries(types)) {
+            //     console.log(key)
+            //     console.log(value)
+            // }
+        })
+    }
+
+    clickUpload(){
+        this.fileInputRef.current.click()
+    }
+
+    onFilesAdded(e){
+        const files = e.target.files;
+        console.log(files)
+
+        
+        this.setState({success: false, msg: ''})
+        
+        // if(!file) {
+        //     return {success: false, msg: 'Please upload a file.'}
+        // }
+        // if(file.size >= 50000000) {
+        //     return {success: false, msg: 'File size exceeds limit of 50MB.'}
+        // }
+        
+        // iterate through images and add them as asset -> get id back from mysqlDB; look for jsons with same name and add them with addDocument and given reference
+        // error when: no metadata file found for image
+        // warning when metadata files without image
+
+        // add types also on this page
+        // show schema also on this page
+
+        QueryHandler.addDocument(files[0], uploadRef)
+            .then(result => {
+                this.setState({success: result.success, msg: result.msg, showMsg: true})
+                setTimeout(() => {
+                    this.setState({showMsg: false})
+                }, result.success ? 3000 : 10000)
+            })
+    }
+
+    render() {
+        const {showMsg, success, msg, typeList} = this.state
+        
+        return (
+        <div>
+             <input
+                hidden
+                ref={this.fileInputRef}
+                className="FileInput"
+                type="file"
+                onChange={this.onFilesAdded}
+            />
+
+                                    
+            <Button className='imageButton' color='black' icon='add' onClick={() => this.clickUpload()}/>
+            {typeList.map(obj => (
+                console.log(key)
+                <Card>
+                    <Card.Content header='About Amy' />
+                    <Card.Content description>
+                        <ReactJson src={obj} enableClipboard={false} displayObjectSize={false} displayDataTypes={false} theme={theme}/>
+                    </Card.Content>
+                    <Card.Content extra>
+                    <Icon name='user' />
+                    4 Friends
+                    </Card.Content>
+                </Card>
+            ))}
+            <div className={showMsg ? 'messageBox' : 'messageBox hidden'}>
+                <Message content={msg} positive={success} negative={!success}/>
+            </div>
+
+        </div>
+        );
+    }
+}
