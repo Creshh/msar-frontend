b53736c8b32c2010b1be0c388559a0054c4a573b
Tom Kretzschmar <tom.kretzschmar@s2015.tu-chemnitz.de>
2019-05-21 16:02:17 +0200

    refactoring upload (errors)

---
+++ src/components/upload/ListComponent.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {List} from 'semantic-ui-react'
+import ListElementAssetComponent from './ListElementAssetComponent';
+
+
+export default class ListComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {status, files} = this.props
+
+        return (
+            <List divided relaxed className='fileList'>
+                {Object.keys(files).map(fileName => (
+                    <ListElementAssetComponent
+                        status={status}
+                        assetFileName={fileName + '.' + files[fileName].fileType}
+                        assetSuccess={files[fileName].success}
+                        metaFileMap={files[fileName].metaFiles}
+                        total={files[fileName].total}
+                        finished={files[fileName].finished}
+                        msg={files[fileName].msg}
+                        removeMetaFile={(metaFileName => (this.props.removeMetaFile(metaFileName)))}
+                    />
+                ))}
+            </List>
+        );
+    }
+}
